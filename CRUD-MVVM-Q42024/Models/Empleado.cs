14f4556395cb121cc882b724a4f8b87a43237188
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-22 18:36:42 -0600

    Add project files.

---
+++ CRUD-MVVM-Q42024/Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿
+using SQLite;
+
+namespace CRUD_MVVM_Q42024.Models
+{
+    public class Empleado
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
