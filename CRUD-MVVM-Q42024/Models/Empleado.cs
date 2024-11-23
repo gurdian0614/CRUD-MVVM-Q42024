31ba13f8429354f0fff5f7f8edaca568e56c58d3
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-22 18:57:37 -0600

    Update Empleado.cs

--- CRUD-MVVM-Q42024/Models/Empleado.cs
+++ CRUD-MVVM-Q42024/Models/Empleado.cs
@@ -10,6 +10,6 @@ namespace CRUD_MVVM_Q42024.Models
         [NotNull]
         public string Nombre { get; set; }
         public string Direccion { get; set; }
-        public string Email { get; set; } 
+        public string Email { get; set; }
     }
 }
