9451e359a892f4c49c084c6970724bbfcb0965be
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-22 19:11:13 -0600

    Adding view models file and views, configuring starter view

---
+++ CRUD-MVVM-Q42024/Views/AddEmpleadoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CRUD_MVVM_Q42024.Views;
+
+public partial class AddEmpleadoView : ContentPage
+{
+	public AddEmpleadoView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CRUD-MVVM-Q42024/Views/MainEmpleadoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CRUD_MVVM_Q42024.Views;
+
+public partial class MainEmpleadoView : ContentPage
+{
+	public MainEmpleadoView()
+	{
+		InitializeComponent();
+	}
+}
