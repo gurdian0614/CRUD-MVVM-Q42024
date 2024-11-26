a863f586048fe3d83dc2061b75ab3264b0a24094
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-25 19:24:11 -0600

    Build view

--- CRUD-MVVM-Q42024/Views/MainEmpleadoView.xaml.cs
+++ CRUD-MVVM-Q42024/Views/MainEmpleadoView.xaml.cs
@@ -1,9 +1,20 @@
+using CRUD_MVVM_Q42024.ViewModels;
+
 namespace CRUD_MVVM_Q42024.Views;
 
 public partial class MainEmpleadoView : ContentPage
 {
+	private MainEmpleadoViewModel viewModel;
 	public MainEmpleadoView()
 	{
 		InitializeComponent();
+		viewModel = new MainEmpleadoViewModel();
+		this.BindingContext = viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		viewModel.GetAll();
+    }
 }
