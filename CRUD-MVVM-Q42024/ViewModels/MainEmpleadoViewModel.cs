9ff0b3cb80c8da45bf92d5bb46b2daee5235d23a
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-25 18:37:46 -0600

    Adding view model

--- CRUD-MVVM-Q42024/ViewModels/MainEmpleadoViewModel.cs
+++ CRUD-MVVM-Q42024/ViewModels/MainEmpleadoViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CRUD_MVVM_Q42024.Models;
+using CRUD_MVVM_Q42024.Services;
+using CRUD_MVVM_Q42024.Views;
+using System.Collections.ObjectModel;
 
 namespace CRUD_MVVM_Q42024.ViewModels
 {
-    internal class MainEmpleadoViewModel
+    public partial class MainEmpleadoViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<Empleado> empleadoCollection = new ObservableCollection<Empleado>();
+
+        private readonly EmpleadoService empleadoService;
+
+        public MainEmpleadoViewModel()
+        {
+            empleadoService = new EmpleadoService();
+        }
+
+        public void GetAll()
+        {
+            var getAll = empleadoService.GetAll();
+
+            if (getAll.Count > 0)
+            {
+                EmpleadoCollection.Clear();
+                foreach (var empleado in getAll)
+                {
+                    EmpleadoCollection.Add(empleado);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToAddEmpleadoView()
+        {
+            await App.Current!.MainPage!.Navigation.PushAsync(new MainEmpleadoView());
+        }
     }
 }
